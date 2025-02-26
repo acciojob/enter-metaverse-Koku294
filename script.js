14ea5408ca2b639d2750da258a0f2ecc3d8dc4bb
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-02-26 20:53:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const enterBtn = document.getElementById('enterBtn');
+        const status = document.getElementById('status');
+
+        // Add event listener to the button for click event
+        enterBtn.addEventListener('click', function() {
+            // Change the content of the paragraph to a new <h1> tag with the new text
+            status.innerHTML = '<h1>Entered Metaverse</h1>';});
+								  
